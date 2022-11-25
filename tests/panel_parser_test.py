979b1b8f5b4411b24e4f3093e996dcab5c3ddb6b
Johan Lundahl <johan.t.lundahl@gmail.com>
2022-11-25 15:05:18 +0100

    added test structure

---
+++ tests/panel_parser_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class PanelParsertest(unittest.TestCase):
+
+    def test_panel_parser(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
